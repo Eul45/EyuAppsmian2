b253ff214572a8243c4ecc5bbb89b9bc2baa403e
Eul45 <nenjose22@gmail.com>
2024-08-31 23:48:27 +0300

    Add files via upload

---
+++ js/script .js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate');
+            }
+        });
+    });
+
+    document.querySelectorAll('.category').forEach(section => {
+        observer.observe(section);
+    });
+});
+
